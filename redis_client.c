24fb49bf1a2940c21c08f7e9096fdcc68f971565
root <peterdir@ohsu.edu>
2022-08-24 18:16:28 +0000

    it's working wih upstash.com

--- redis_client.c
+++ redis_client.c
@@ -37,16 +37,38 @@ int redis_lookup(const char *service, const char *tenant, const char *name, char
     printf( "@ %s::(in) name=%s\n", __FUNCTION__, name ) ;
 #endif
 
-#if defined(REDIS_SOCKET)
-    c = redisConnectUnixWithTimeout(REDIS_SOCKET, timeout);
-#else
-	c = redisConnectWithTimeout(REDIS_HOST, REDIS_PORT, timeout);
-#endif
+// #if defined(REDIS_SOCKET)
+//    c = redisConnectUnixWithTimeout(REDIS_SOCKET, timeout);
+// #else
+    c = redisConnectWithTimeout(REDIS_HOST, REDIS_PORT, timeout);
+// #endif
+
     if (c == NULL || c->err) {
-		redisFree(c);
-        goto fail;
+        if (c) {
+            printf("Connection error: %s\n", c->errstr);
+            redisFree(c);
+        } else {
+            printf("Connection error: can't allocate redis context\n");
+        }
+	goto fail;
+        // exit(1);
     }
 
+
+    // AUTH
+    reply = redisCommand(c,"AUTH %s", REDIS_PASSWORD);
+    if (reply == NULL) {
+        // wrong redis password
+	printf("wrong redis password");
+        //// ap_log_error(APLOG_MARK, APLOG_ERR, 0, s,"wrong redis password");
+        //// free_redis_ctx(ctx, s);
+        redisFree(c);
+        goto fail;
+        //// return NULL;
+    }
+    // DEBUG
+    //ap_log_error(APLOG_MARK, APLOG_ERR, 0, s,"redis reply: AUTH, type[%d], str[%s]", reply->type, reply->str);
+    freeReplyObject(reply);
 
 #if DEBUG
 #if USE_TENANT
@@ -68,9 +90,25 @@ int redis_lookup(const char *service, const char *tenant, const char *name, char
     reply = redisCommand(c,"GET %s/%s/%s", service, tenant, name);
 #else
     reply = redisCommand(c,"GET %s/%s", service, name);
-#endif
-
-
+#endif    
+    if (c == NULL || c->err) {
+	#if DEBUG
+        if (c) {
+	    char *ret;	
+	    ret = strstr(c->errstr, "erver closed the connectio");
+	    if (ret) {
+	      printf("Server closed the connection, have you exceeded a quota?\n");
+            } else {
+              printf("Connection error : %s\n", c->errstr);
+	    }
+            redisFree(c);
+        } else {
+            printf("Connection error: can't allocate redis context\n");
+        }
+	#endif
+        goto fail;
+        // exit(1);
+    }
 #if DEBUG
     printf("@ %s::(out) value=%s\n", __FUNCTION__, reply->str);
 #endif
